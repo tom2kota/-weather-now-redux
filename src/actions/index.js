85b554b570d07c73b7d70916a7d6bd7a386bda20
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-05-13 14:01:47 +0200

    Add files via upload

---
+++ src/actions/index.js
@@ -0,0 +1,49 @@
+import {SET_DEFAULT_CITY, SET_SEARCH_CITY} from './types';
+
+import openweathermap from '../api/openweathermap';
+
+export const fetchDefaultCity = () => async dispatch => {
+    console.log('fetchDefaultCity()');
+
+    const response = await openweathermap.get('/weather1')
+    dispatch({type: SET_DEFAULT_CITY, payload: response.data})
+}
+
+// export function fetchDefaultCityOld() {
+//
+//     return function (dispatch) {
+//         openweathermap.get('/weather')
+//             .then(response => dispatch({
+//                 type: SET_DEFAULT_CITY,
+//                 payload: response.data
+//             }))
+//     }
+// }
+
+
+export function fetchSearchCity(query, callback) {
+    return function (dispatch) {
+        openweathermap.get('/weather', {params: {q: query}})
+            .then(response => {
+                dispatch({
+                    type: SET_SEARCH_CITY,
+                    payload: response.data
+                })
+                if (callback) {
+                    callback()
+                }
+            })
+    }
+}
+
+
+export const fetchCity = (searchRegexTerm) => async dispatch => {
+    const response = await openweathermap.get('/weather',
+        {
+            params: {
+                q: searchRegexTerm
+            }
+        })
+
+    dispatch({type: SET_SEARCH_CITY, payload: response.data})
+}
---
+++ src/actions/types.js
@@ -0,0 +1,2 @@
+export const SET_DEFAULT_CITY = 'SET_DEFAULT_CITY';
+export const SET_SEARCH_CITY = 'SET_SEARCH_CITY';
