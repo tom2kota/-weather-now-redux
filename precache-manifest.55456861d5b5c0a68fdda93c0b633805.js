7d88b9e4b1a7a5b3711974fbd589bb177b362ddc
tom2kota <tom2kota@gmail.com>
2020-05-13 12:15:04 +0000

    Updates

---
+++ precache-manifest.55456861d5b5c0a68fdda93c0b633805.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ccd59c25e77250e8b62015d363bcfd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0af2ccb5a4f08f06b690",
+    "url": "./static/css/main.95334241.chunk.css"
+  },
+  {
+    "revision": "bd4fc616a3dce6e31ab7",
+    "url": "./static/js/2.60093afc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.60093afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0af2ccb5a4f08f06b690",
+    "url": "./static/js/main.adba7d87.chunk.js"
+  },
+  {
+    "revision": "30be6598de8b3eba63a6",
+    "url": "./static/js/runtime-main.cf785097.js"
+  },
+  {
+    "revision": "19461b5bfc69ee6c637d55f7f5932a4f",
+    "url": "./static/media/weather-logo.19461b5b.svg"
+  }
+]);
