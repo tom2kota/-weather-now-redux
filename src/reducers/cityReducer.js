f62e0e4f9268632289d5fbafd06c3e0a6ebfb14b
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-05-14 03:37:00 +0300

    Update cityReducer.js

--- src/reducers/cityReducer.js
+++ src/reducers/cityReducer.js
@@ -1,13 +1,18 @@
-import {SET_DEFAULT_CITY} from "../actions/types";
+import {SET_DEFAULT_CITY, SET_SEARCH_CITY, SET_FETCH_ERROR} from "../actions/types";
 
 const INIT_STATE = {
     defaultCity: [],
-    searchCity: []
+    searchCity: [],
+    fetchError: []
 }
 
 export default (state = INIT_STATE, action) => {
     switch (action.type) {
         case SET_DEFAULT_CITY:
+            return action.payload
+        case SET_SEARCH_CITY:
+            return action.payload
+        case SET_FETCH_ERROR:
             return action.payload
         default:
             return state
